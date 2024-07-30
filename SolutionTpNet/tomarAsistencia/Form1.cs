33fd7747ec253a2c8549505553b681c3b319b030
Marina Milo <milomarina51@gmail.com>
2024-07-30 19:12:46 -0300

    Crear prototipo tomarAsistencia
    Prototipo del form de Tomar Asistencia

--- SolutionTpNet/tomarAsistencia/Form1.cs
+++ SolutionTpNet/tomarAsistencia/Form1.cs
@@ -16,5 +16,35 @@ namespace tomarAsistencia
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlPresentes_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
