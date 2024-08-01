576540abb868ce411c76212e9fe0be9b8f5eb05d
Marina Milo <milomarina51@gmail.com>
2024-08-01 11:58:46 -0300

    Corregir Interfaz TomarAsistencia
    Se unieron las DataGrid en una sola para todos los estudiantes, tanto ausentes como presentes, en orden alfabético

--- SolutionTpNet/tomarAsistencia/Form1.Designer.cs
+++ SolutionTpNet/tomarAsistencia/Form1.Designer.cs
@@ -29,30 +29,26 @@
         private void InitializeComponent()
         {
             this.pnlScroll = new System.Windows.Forms.Panel();
+            this.btnTerminarClase = new System.Windows.Forms.Button();
+            this.btnGuardarAsistencia = new System.Windows.Forms.Button();
             this.lblFecha = new System.Windows.Forms.Label();
             this.pnlTop = new System.Windows.Forms.Panel();
             this.picPerfil = new System.Windows.Forms.PictureBox();
             this.lblNombreApp = new System.Windows.Forms.Label();
             this.picLogo = new System.Windows.Forms.PictureBox();
             this.lblNombreAsignatura = new System.Windows.Forms.Label();
-            this.lblPresentes = new System.Windows.Forms.Label();
-            this.txtBusquedaPresentes = new System.Windows.Forms.TextBox();
             this.pnlPresentes = new System.Windows.Forms.Panel();
             this.dgvPresentes = new System.Windows.Forms.DataGridView();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.dgvAusentes = new System.Windows.Forms.DataGridView();
-            this.lblAusentes = new System.Windows.Forms.Label();
-            this.txtBusquedaAusentes = new System.Windows.Forms.TextBox();
-            this.btnGuardarAsistencia = new System.Windows.Forms.Button();
-            this.btnTerminarClase = new System.Windows.Forms.Button();
+            this.txtBusquedaPresentes = new System.Windows.Forms.TextBox();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnConfiguración = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.pnlScroll.SuspendLayout();
             this.pnlTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picPerfil)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.pnlPresentes.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPresentes)).BeginInit();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvAusentes)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlScroll
@@ -64,12 +60,36 @@
             this.pnlScroll.Controls.Add(this.pnlTop);
             this.pnlScroll.Controls.Add(this.lblNombreAsignatura);
             this.pnlScroll.Controls.Add(this.pnlPresentes);
-            this.pnlScroll.Controls.Add(this.panel2);
             this.pnlScroll.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlScroll.Location = new System.Drawing.Point(0, 0);
             this.pnlScroll.Name = "pnlScroll";
             this.pnlScroll.Size = new System.Drawing.Size(804, 749);
             this.pnlScroll.TabIndex = 0;
+            // 
+            // btnTerminarClase
+            // 
+            this.btnTerminarClase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
+            this.btnTerminarClase.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTerminarClase.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
+            this.btnTerminarClase.Location = new System.Drawing.Point(590, 685);
+            this.btnTerminarClase.Name = "btnTerminarClase";
+            this.btnTerminarClase.Size = new System.Drawing.Size(147, 36);
+            this.btnTerminarClase.TabIndex = 15;
+            this.btnTerminarClase.Text = "Terminar clase";
+            this.btnTerminarClase.UseVisualStyleBackColor = false;
+            this.btnTerminarClase.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGuardarAsistencia
+            // 
+            this.btnGuardarAsistencia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(161)))), ((int)(((byte)(227)))));
+            this.btnGuardarAsistencia.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarAsistencia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
+            this.btnGuardarAsistencia.Location = new System.Drawing.Point(559, 643);
+            this.btnGuardarAsistencia.Name = "btnGuardarAsistencia";
+            this.btnGuardarAsistencia.Size = new System.Drawing.Size(178, 36);
+            this.btnGuardarAsistencia.TabIndex = 14;
+            this.btnGuardarAsistencia.Text = "Guardar asistencia";
+            this.btnGuardarAsistencia.UseVisualStyleBackColor = false;
             // 
             // lblFecha
             // 
@@ -85,6 +105,9 @@
             // pnlTop
             // 
             this.pnlTop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTop.Controls.Add(this.btnBuscar);
+            this.pnlTop.Controls.Add(this.btnConfiguración);
+            this.pnlTop.Controls.Add(this.btnAyuda);
             this.pnlTop.Controls.Add(this.picPerfil);
             this.pnlTop.Controls.Add(this.lblNombreApp);
             this.pnlTop.Controls.Add(this.picLogo);
@@ -131,110 +154,57 @@
             this.lblNombreAsignatura.TabIndex = 10;
             this.lblNombreAsignatura.Text = "NET 3EK01 - Tecnologías de Desarrollo de Software IDE";
             // 
-            // lblPresentes
+            // pnlPresentes
             // 
-            this.lblPresentes.AutoSize = true;
-            this.lblPresentes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPresentes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
-            this.lblPresentes.Location = new System.Drawing.Point(3, 3);
-            this.lblPresentes.Name = "lblPresentes";
-            this.lblPresentes.Size = new System.Drawing.Size(90, 18);
-            this.lblPresentes.TabIndex = 4;
-            this.lblPresentes.Text = "Presentes";
+            this.pnlPresentes.Controls.Add(this.dgvPresentes);
+            this.pnlPresentes.Controls.Add(this.txtBusquedaPresentes);
+            this.pnlPresentes.Location = new System.Drawing.Point(80, 192);
+            this.pnlPresentes.Name = "pnlPresentes";
+            this.pnlPresentes.Size = new System.Drawing.Size(657, 428);
+            this.pnlPresentes.TabIndex = 12;
+            // 
+            // dgvPresentes
+            // 
+            this.dgvPresentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPresentes.Location = new System.Drawing.Point(6, 39);
+            this.dgvPresentes.Name = "dgvPresentes";
+            this.dgvPresentes.Size = new System.Drawing.Size(648, 386);
+            this.dgvPresentes.TabIndex = 6;
             // 
             // txtBusquedaPresentes
             // 
             this.txtBusquedaPresentes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
             this.txtBusquedaPresentes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBusquedaPresentes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(122)))), ((int)(((byte)(148)))));
-            this.txtBusquedaPresentes.Location = new System.Drawing.Point(6, 36);
+            this.txtBusquedaPresentes.Location = new System.Drawing.Point(6, 7);
             this.txtBusquedaPresentes.Name = "txtBusquedaPresentes";
             this.txtBusquedaPresentes.Size = new System.Drawing.Size(648, 26);
             this.txtBusquedaPresentes.TabIndex = 5;
             this.txtBusquedaPresentes.Text = "Buscar estudiantes";
             // 
-            // pnlPresentes
+            // btnAyuda
             // 
-            this.pnlPresentes.Controls.Add(this.dgvPresentes);
-            this.pnlPresentes.Controls.Add(this.lblPresentes);
-            this.pnlPresentes.Controls.Add(this.txtBusquedaPresentes);
-            this.pnlPresentes.Location = new System.Drawing.Point(80, 192);
-            this.pnlPresentes.Name = "pnlPresentes";
-            this.pnlPresentes.Size = new System.Drawing.Size(657, 199);
-            this.pnlPresentes.TabIndex = 12;
+            this.btnAyuda.Location = new System.Drawing.Point(693, 9);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(40, 40);
+            this.btnAyuda.TabIndex = 3;
+            this.btnAyuda.UseVisualStyleBackColor = true;
             // 
-            // dgvPresentes
+            // btnConfiguración
             // 
-            this.dgvPresentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPresentes.Location = new System.Drawing.Point(6, 78);
-            this.dgvPresentes.Name = "dgvPresentes";
-            this.dgvPresentes.Size = new System.Drawing.Size(648, 115);
-            this.dgvPresentes.TabIndex = 6;
+            this.btnConfiguración.Location = new System.Drawing.Point(645, 9);
+            this.btnConfiguración.Name = "btnConfiguración";
+            this.btnConfiguración.Size = new System.Drawing.Size(40, 40);
+            this.btnConfiguración.TabIndex = 4;
+            this.btnConfiguración.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // btnBuscar
             // 
-            this.panel2.Controls.Add(this.dgvAusentes);
-            this.panel2.Controls.Add(this.lblAusentes);
-            this.panel2.Controls.Add(this.txtBusquedaAusentes);
-            this.panel2.Location = new System.Drawing.Point(80, 420);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(657, 199);
-            this.panel2.TabIndex = 13;
-            // 
-            // dgvAusentes
-            // 
-            this.dgvAusentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAusentes.Location = new System.Drawing.Point(6, 78);
-            this.dgvAusentes.Name = "dgvAusentes";
-            this.dgvAusentes.Size = new System.Drawing.Size(648, 115);
-            this.dgvAusentes.TabIndex = 6;
-            // 
-            // lblAusentes
-            // 
-            this.lblAusentes.AutoSize = true;
-            this.lblAusentes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAusentes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
-            this.lblAusentes.Location = new System.Drawing.Point(3, 3);
-            this.lblAusentes.Name = "lblAusentes";
-            this.lblAusentes.Size = new System.Drawing.Size(84, 18);
-            this.lblAusentes.TabIndex = 4;
-            this.lblAusentes.Text = "Ausentes";
-            // 
-            // txtBusquedaAusentes
-            // 
-            this.txtBusquedaAusentes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
-            this.txtBusquedaAusentes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBusquedaAusentes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(122)))), ((int)(((byte)(148)))));
-            this.txtBusquedaAusentes.Location = new System.Drawing.Point(6, 36);
-            this.txtBusquedaAusentes.Name = "txtBusquedaAusentes";
-            this.txtBusquedaAusentes.Size = new System.Drawing.Size(648, 26);
-            this.txtBusquedaAusentes.TabIndex = 5;
-            this.txtBusquedaAusentes.Text = "Buscar estudiantes";
-            // 
-            // btnGuardarAsistencia
-            // 
-            this.btnGuardarAsistencia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(161)))), ((int)(((byte)(227)))));
-            this.btnGuardarAsistencia.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardarAsistencia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
-            this.btnGuardarAsistencia.Location = new System.Drawing.Point(559, 628);
-            this.btnGuardarAsistencia.Name = "btnGuardarAsistencia";
-            this.btnGuardarAsistencia.Size = new System.Drawing.Size(178, 36);
-            this.btnGuardarAsistencia.TabIndex = 14;
-            this.btnGuardarAsistencia.Text = "Guardar asistencia";
-            this.btnGuardarAsistencia.UseVisualStyleBackColor = false;
-            // 
-            // btnTerminarClase
-            // 
-            this.btnTerminarClase.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
-            this.btnTerminarClase.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTerminarClase.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(23)))), ((int)(((byte)(28)))));
-            this.btnTerminarClase.Location = new System.Drawing.Point(590, 670);
-            this.btnTerminarClase.Name = "btnTerminarClase";
-            this.btnTerminarClase.Size = new System.Drawing.Size(147, 36);
-            this.btnTerminarClase.TabIndex = 15;
-            this.btnTerminarClase.Text = "Terminar clase";
-            this.btnTerminarClase.UseVisualStyleBackColor = false;
-            this.btnTerminarClase.Click += new System.EventHandler(this.button2_Click);
+            this.btnBuscar.Location = new System.Drawing.Point(597, 9);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(40, 40);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.UseVisualStyleBackColor = true;
             // 
             // frmTomarAsistencia
             // 
@@ -258,9 +228,6 @@
             this.pnlPresentes.ResumeLayout(false);
             this.pnlPresentes.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPresentes)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvAusentes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -276,14 +243,12 @@
         private System.Windows.Forms.Label lblNombreAsignatura;
         private System.Windows.Forms.Panel pnlPresentes;
         private System.Windows.Forms.DataGridView dgvPresentes;
-        private System.Windows.Forms.Label lblPresentes;
         private System.Windows.Forms.TextBox txtBusquedaPresentes;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.DataGridView dgvAusentes;
-        private System.Windows.Forms.Label lblAusentes;
-        private System.Windows.Forms.TextBox txtBusquedaAusentes;
         private System.Windows.Forms.Button btnGuardarAsistencia;
         private System.Windows.Forms.Button btnTerminarClase;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnConfiguración;
     }
 }
 
